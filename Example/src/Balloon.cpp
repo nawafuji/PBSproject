84d551d5a0c2712fa40fecd7794c3cee96b7a062
Eisuke Fujinawa <nawafuji@gmail.com>
2015-11-15 19:23:19 +0100

    first commit

---
+++ Example/src/Balloon.cpp
@@ -0,0 +1,35 @@
+#include "Balloon.h"
+
+Balloon::Balloon(double r)
+{
+    positions = Eigen::MatrixXd(3, 4);
+    
+    positions.col(0) << 0.0, 0.0, 5.0;
+    positions.col(1) << 5.0, 5.0, 5.0;
+    positions.col(2) << 5.0, 0.0, 5.0;
+    positions.col(3) << 0.0, 5.0, 5.0;
+    std::cout << positions << std::endl;
+    radius = r;
+}
+
+void Balloon::render()
+{
+    glColor3f(0.5,0.5,0.5);
+    for(int i=0; i<positions.cols(); i++)
+    {
+      glPushMatrix();
+      {
+        glTranslated(positions(0,i), positions(1,i), positions(2,i));
+        glutSolidSphere(radius, 50, 50);
+      }
+      glPopMatrix();
+    }
+}
+
+void Balloon::update()
+{
+    for(int i=0; i<positions.cols(); i++)
+    {
+      positions.col(i) += Eigen::Vector3d(0.01, 0.01, 0.01);
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+how to use example
+
+1. Download Eigen and place it in the same directory with Example and GLUT
+
+2. cd Example
+   mkdir build
+   cd build
+   cmake ..
+   make
+   ./Example
